a442aab726f61bc5cc61cc2af375b07dd2824c51
Hamed Shahidi <hamed.shahidiz@gmail.com>
2023-06-18 21:06:08 +0300

    Adds and configures redis to be used for caching

---
+++ backend/src/main/java/hamedshahidi/wordfrequency/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package hamedshahidi.wordfrequency.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Autowired
+    public RedisConfig(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
