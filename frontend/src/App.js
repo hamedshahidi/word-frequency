3fda8eea7a0a2f1c756f77e337b48abf187721d9
Hamed Shahidi <hamed.shahidiz@gmail.com>
2023-06-19 08:16:02 +0300

    Adds progressbar to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [kValue, setKValue] = useState('');
   const [wordFrequency, setWordFrequency] = useState([]);
   const [processingChunks, setProcessingChunks] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [processingProgress, setProcessingProgress] = useState(0);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -43,33 +47,39 @@ function App() {
       formData.append('file', new Blob([chunk]));
       formData.append('k', kValue);
       formData.append('offset', offset);
-  
+    
       // Send the chunk to the backend API
       axios
         .post('/upload-chunk', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
+          onUploadProgress: (progressEvent) => {
+            const progress = Math.round((100 * progressEvent.loaded) / progressEvent.total);
+            setUploadProgress(progress);
+          },
         })
         .then((response) => {
           // Handle response from the backend API
           console.log(response.data);
-  
+    
           // Update the offset for the next chunk
           offset += chunkSize;
-  
+    
           // Check if there are more chunks to read
           if (offset < fileSize) {
             readNextChunk();
           } else {
             // All chunks have been processed
             setProcessingChunks(false);
-            fetchData()
+            setUploadProgress(0); // Reset upload progress
+            fetchData();
           }
         })
         .catch((error) => {
           console.error(error);
           setProcessingChunks(false);
+          setUploadProgress(0); // Reset upload progress
           // Handle error from the backend API
         });
     };
@@ -90,14 +100,18 @@ function App() {
     const formData = new FormData();
     formData.append('file', selectedFile);
     formData.append('k', kValue);
-
+  
     try {
       const response = await axios.post('/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
+        onUploadProgress: (progressEvent) => {
+          const progress = Math.round((100 * progressEvent.loaded) / progressEvent.total);
+          setProcessingProgress(progress);
+        },
       });
-
+  
       // Handle response from the backend API
       const words = response.data.words;
       const frequencies = response.data.frequencies;
@@ -110,6 +124,8 @@ function App() {
     } catch (error) {
       console.error(error);
       // Handle error from the backend API
+    } finally {
+      setProcessingProgress(0); // Reset processing progress
     }
   };
 
@@ -117,29 +133,49 @@ function App() {
     <div className="container">
       <h1 className="mt-5">Top K Words</h1>
       <form onSubmit={handleSubmit}>
+        {/* File input */}
         <div className="mb-3">
           <label htmlFor="fileInput" className="form-label">
             Select File:
           </label>
           <input type="file" className="form-control" id="fileInput" onChange={handleFileChange} />
         </div>
+  
+        {/* K value input */}
         <div className="mb-3">
           <label htmlFor="kValueInput" className="form-label">
             K Value:
           </label>
           <input type="number" className="form-control" id="kValueInput" value={kValue} onChange={handleKValueChange} />
         </div>
+  
+        {/* Submit button */}
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+  
+        {/* Upload progress bar */}
+        {processingChunks && (
+          <ProgressBar now={uploadProgress} label={`${uploadProgress}%`} className="mt-3" animated striped />
+        )}
+  
+        {/* Processing progress bar */}
+        {!processingChunks && processingProgress > 0 && (
+          <ProgressBar now={processingProgress} label={`${processingProgress}%`} className="mt-3" animated striped />
+        )}
       </form>
+  
+      {/* Word frequency table */}
       <table className="table mt-4">
+        {/* Table headers */}
         <thead>
           <tr>
             <th>Word</th>
             <th>Frequency</th>
           </tr>
         </thead>
+  
+        {/* Table body */}
         <tbody>
           {wordFrequency.map((entry) => (
             <tr key={entry.word}>
